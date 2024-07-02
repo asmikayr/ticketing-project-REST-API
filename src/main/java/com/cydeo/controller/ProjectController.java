2ca6b01ba3347be02b92b28668acd2b5a3ba1277
Asmik <jasmin_ayr@mail.ru>
2024-07-02 23:52:33 +0200

    RolesAllowed added

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.List;
 
 @RestController
@@ -20,6 +21,7 @@ public class ProjectController{
     }
 
     @GetMapping
+    @RolesAllowed("Manager")
     public ResponseEntity<ResponseWrapper> getProjects(){
         List<ProjectDTO> projects = projectService.listAllProjects();
 
@@ -28,12 +30,14 @@ public class ProjectController{
     }
 
     @GetMapping("/{projectCode}")
+    @RolesAllowed("Admin")
     public ResponseEntity<ResponseWrapper> getProjectByCode(@PathVariable ("projectCode") String projectCode){
         ProjectDTO project = projectService.getByProjectCode(projectCode);
         return ResponseEntity.ok(new ResponseWrapper(true, "Project successfully retrieved", HttpStatus.OK.value(), project));
     }
 
     @PostMapping
+    @RolesAllowed({"Admin", "Manager"})
     public ResponseEntity<ResponseWrapper> createProject(@RequestBody ProjectDTO projectDTO){
         projectService.save(projectDTO);
 
@@ -41,6 +45,7 @@ public class ProjectController{
     }
 
     @PutMapping
+    @RolesAllowed("Manager")
     public ResponseEntity<ResponseWrapper> updateProject(@RequestBody ProjectDTO projectDTO){
         projectService.update(projectDTO);
 
@@ -48,17 +53,20 @@ public class ProjectController{
     }
 
     @DeleteMapping("/{projectCode}")
+    @RolesAllowed("Manager")
     public ResponseEntity<ResponseWrapper> deletePorject(@PathVariable ("projectCode") String projectCode){
     projectService.delete(projectCode);
     return ResponseEntity.ok(new ResponseWrapper("project is successfully deleted", HttpStatus.OK));
     }
 
     @GetMapping("/manager/project-status")
+    @RolesAllowed("Manager")
     public ResponseEntity<ResponseWrapper> getProjectByManager(){
         List<ProjectDTO> projectDTOList = projectService.listAllProjectDetails();
         return ResponseEntity.ok(new ResponseWrapper("Projects are successfully retrieved", projectDTOList, HttpStatus.OK));
     }
     @PutMapping("/manager/complete/{projectCode}")
+    @RolesAllowed("Manager")
     public ResponseEntity<ResponseWrapper> managerCompleteProject(@PathVariable("projectCode") String projectCode){
         projectService.complete(projectCode);
         return ResponseEntity.ok(new ResponseWrapper("Project is successfully completed", HttpStatus.OK));
--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.List;
 
 @RestController
@@ -19,6 +20,7 @@ public class UserController {
         this.userService = userService;
     }
     @GetMapping
+    @RolesAllowed({"Manager", "Admin"})
     public ResponseEntity<ResponseWrapper> getUsers(){
         List<UserDTO> users = userService.listAllUsers();
         ResponseWrapper responseWrapper = new ResponseWrapper();
@@ -33,6 +35,7 @@ public class UserController {
     }
 
     @GetMapping("/{username}")
+    @RolesAllowed("Admin")
     public ResponseEntity<ResponseWrapper> getUserByUserName(@PathVariable("username") String username){
 
         UserDTO user = userService.findByUserName(username);
@@ -40,6 +43,7 @@ public class UserController {
         return ResponseEntity.ok(new ResponseWrapper("User is successfully retrieved", user, HttpStatus.OK));
     }
     @PostMapping
+    @RolesAllowed("Admin")
     public ResponseEntity<ResponseWrapper> createUser(@RequestBody UserDTO user){
         userService.save(user);
 
@@ -47,11 +51,13 @@ public class UserController {
 
     }
     @PutMapping
+    @RolesAllowed("Admin")
     public ResponseEntity<ResponseWrapper> updateUser(@RequestBody UserDTO user){
         userService.update(user);
         return ResponseEntity.ok(new ResponseWrapper("User is updated", HttpStatus.OK));
     }
     @DeleteMapping("/{username}")
+    @RolesAllowed("Admin")
     public ResponseEntity<ResponseWrapper> deleteUser(@PathVariable("username") String username){
         userService.delete(username);
         return  ResponseEntity.ok(new ResponseWrapper("User is successfully deleted", HttpStatus.OK));
