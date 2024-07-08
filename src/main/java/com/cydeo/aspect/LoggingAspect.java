df26432561a54a9547e4699017a3076bd492ae9b
Asmik <jasmin_ayr@mail.ru>
2024-07-08 16:25:42 +0200

    Logging Aspect created

---
+++ src/main/java/com/cydeo/aspect/LoggingAspect.java
@@ -0,0 +1,13 @@
+package com.cydeo.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j // Logger
+public class LoggingAspect {
+
+
+}
