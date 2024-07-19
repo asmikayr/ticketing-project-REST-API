30cb90ae457a2c009a0dcd9ab94d615808617901
Asmik <jasmin_ayr@mail.ru>
2024-07-19 21:32:43 +0200

    ProjectControllerTest - givenToken test added

--- src/test/java/com/cydeo/controller/ProjectControllerTest.java
+++ src/test/java/com/cydeo/controller/ProjectControllerTest.java
@@ -6,14 +6,18 @@ import com.cydeo.dto.UserDTO;
 import com.cydeo.enums.Gender;
 import com.cydeo.enums.Status;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -21,11 +25,29 @@ class ProjectControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    static String token;
+
     static UserDTO manager;
     static ProjectDTO project;
     @BeforeAll
     static void setUp(){
+        token = "Bearer " + "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJCbm1VOTg0Z1hibTl1bzBaR29zOGRjcjBNX3RCZy1PR1NYNXpVY3ZORUZzIn0.eyJleHAiOjE3MjE0MzU0MTYsImlhdCI6MTcyMTQxNzQxNiwianRpIjoiMTI5N2YxZjYtYzkxYS00ODk4LWExYTItNjJlNDRlY2Y3YWQ3IiwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo4MDgwL2F1dGgvcmVhbG1zL2N5ZGVvLWRldiIsImF1ZCI6ImFjY291bnQiLCJzdWIiOiI3NzZlZTI4Yy0wYjkyLTQzY2MtYmQ2MS1hOTI2MDFmODEyYmQiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJ0aWNrZXRpbmctYXBwIiwic2Vzc2lvbl9zdGF0ZSI6ImUwMTVhYzVlLTc2ZjMtNDYyNC04ZTU5LTE4ZDhhZWI3ZDNiYiIsImFjciI6IjEiLCJhbGxvd2VkLW9yaWdpbnMiOlsiaHR0cDovL2xvY2FsaG9zdDo4MDgxIl0sInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJvZmZsaW5lX2FjY2VzcyIsInVtYV9hdXRob3JpemF0aW9uIiwiZGVmYXVsdC1yb2xlcy1jeWRlby1kZXYiXX0sInJlc291cmNlX2FjY2VzcyI6eyJ0aWNrZXRpbmctYXBwIjp7InJvbGVzIjpbIk1hbmFnZXIiXX0sImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJtYW5hZ2UtYWNjb3VudC1saW5rcyIsInZpZXctcHJvZmlsZSJdfX0sInNjb3BlIjoib3BlbmlkIGVtYWlsIHByb2ZpbGUiLCJzaWQiOiJlMDE1YWM1ZS03NmYzLTQ2MjQtOGU1OS0xOGQ4YWViN2QzYmIiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwicHJlZmVycmVkX3VzZXJuYW1lIjoib3p6eSJ9.TtE881FzxM3OE2gL_7f5-g_J0oQbbt1IHrry6BkYTNvQt8sEXfTFohgTFwObIZuY6muZR8Fa5mmK3ggYk9uVvxxp6WA5-a8zDH_WCUdJBjCWk-sLqqL8OzVP4KSXyCq6_6GMdE-I9m8h023rYbv3eALCVIILMEKUeFdVEoDLczlbXRVzomyborTn9MA4WXdlGV5CcBzZeKYdNWuYGjEIDEGkyxUrR__aYwfDZbyBhtsH5t2sAcGEQSc2CAZ4MAp2bmKT01vyRTRJkTP48OFduKgvCFLDz1Obrq4SSzIlb-BLxxWoOJjAtUn6yzt1aQuHq5-FAPXdki7lpNoYdimHMA";
         manager = new UserDTO(2L, "","", "ozzy", "abc1", "", true, "", new RoleDTO(2L, "Manager"), Gender.MALE);
         project = new ProjectDTO("API Project", "PR001", manager, LocalDate.now(), LocalDate.now().plusDays(5), "Some details", Status.OPEN);
+    }
+
+    @Test
+    void givenNoToken_getProjects() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/api/v1/project"))
+                .andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    void givenToken_getProjects() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/api/v1/project")
+                .header("Authorization", token)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
     }
 }
