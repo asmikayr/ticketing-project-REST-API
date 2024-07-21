111c17ab2c62b35565cc5cae062fae018cd31d11
Asmik <jasmin_ayr@mail.ru>
2024-07-21 18:00:22 +0200

    UserServiceImplTest in review is created

---
+++ src/test/java/com/cydeo/review/UserServiceImplTest.java
@@ -0,0 +1,38 @@
+package com.cydeo.review;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceImplTest {
+
+    @BeforeAll
+    public static void setUpBeforeClass() throws Exception{
+        //Some impl
+    }
+
+    @AfterAll
+    public static void tearDownAfterClass() throws Exception{
+        //some impl
+    }
+
+    @BeforeEach
+    public void setUp() throws Exception{
+        //Some impl
+    }
+
+    @AfterEach
+    public void tearDown() throws Exception{
+        //Some impl
+    }
+
+    //TestNG
+
+    @Test
+    public void test(){
+
+    }
+
+
+}
